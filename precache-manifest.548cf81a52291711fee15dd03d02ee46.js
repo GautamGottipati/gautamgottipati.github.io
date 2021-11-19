8bb7a577a5a46da91447626e63370ef0c555916d
GautamGottipati <gautamrace@gmail.com>
2021-11-19 22:38:15 +0530

    Updates

---
+++ precache-manifest.548cf81a52291711fee15dd03d02ee46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff2e89e9104fdaa7f12b5ef19919f34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "349a5ea2fde013110631",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349a5ea2fde013110631",
+    "url": "/static/js/main.a14a9050.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "21d9facb17e848038201ed8b81887f81",
+    "url": "/static/media/epam.21d9facb.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "f91ff3fab7f523d75f39dd896a0e2d31",
+    "url": "/static/media/tcs.f91ff3fa.png"
+  }
+]);
